7f734c463fc1259f87250187825bc5608d21e4a2
MLtttttt <1764996121@qq.com>
2026-04-04 01:39:55 +0800

    Delete LCD学习仿真 directory

--- LCD学习仿真/code/LCD1602.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LCD1602_H__
-#define __LCD1602_H__
-
-void LCD_Init(void);
-void LCD_ShowChar(unsigned char Line, unsigned char Column, unsigned char Char);
-void LCD_ShowString(unsigned char row, unsigned char col, unsigned char *str);
-void LCD_WriteCGRAM(unsigned char index, unsigned char *pattern);
-
-#endif
